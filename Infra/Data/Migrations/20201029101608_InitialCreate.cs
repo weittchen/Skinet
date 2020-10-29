8e666df6ffe9a6b538f42d86715e5cd9faf153f0
WeitingChen <weitingchen@chenweitingde-MacBook-Pro.local>
2020-10-29 18:19:38 +0800

    Add ProductConfiguration

--- Infra/Data/Migrations/20201029101608_InitialCreate.cs
+++ Infra/Data/Migrations/20201029101608_InitialCreate.cs
@@ -38,10 +38,10 @@ namespace Infra.Data.Migrations
                 {
                     Id = table.Column<int>(nullable: false)
                         .Annotation("Sqlite:Autoincrement", true),
-                    Name = table.Column<string>(nullable: true),
-                    Description = table.Column<string>(nullable: true),
-                    Price = table.Column<decimal>(nullable: false),
-                    PictureUrl = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,3)", nullable: false),
+                    PictureUrl = table.Column<string>(nullable: false),
                     ProductTypeId = table.Column<int>(nullable: false),
                     ProductBrandId = table.Column<int>(nullable: false)
                 },
